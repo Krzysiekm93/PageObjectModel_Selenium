d9a3f17277c281dd28b5e72cc97f28dcd8358aa9
Krzysiekm93 <metrakkrzysztof@gmail.com>
2026-03-14 14:29:24 +0000

    Added create_account_page logic

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -6,5 +6,8 @@ class RegistrationTest(BaseTest):
         super().setUp()
         self.authentication_page = self.home_page.click_sign_in()
         self.authentication_page.enter_create_account_email("asd@gmail.com")
-    def testNoSurname(self):
+        self.create_account_page = self.authentication_page.click_create_account()
+
+    def testNoLastName(self):
+        self.create_account_page.enter_first_name("Marcin")
         sleep(5)
