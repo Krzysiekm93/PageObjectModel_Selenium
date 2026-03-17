1c346d7d3438d95e4aa5ca47a9070adfba73c63a
Krzysiekm93 <metrakkrzysztof@gmail.com>
2026-03-17 18:04:20 +0000

    Finished DDT example

--- tests/registration_test.py
+++ tests/registration_test.py
@@ -1,8 +1,11 @@
+import datetime
+
 import test_data.registration_data
 from tests.base_test import BaseTest
 from time import sleep
 from test_data.registration_data import RegistrationDataGenerator
 from ddt import ddt, data, unpack
+import unittest
 
 @ddt
 class RegistrationTest(BaseTest):
@@ -10,17 +13,18 @@ class RegistrationTest(BaseTest):
         super().setUp()
         self.data = RegistrationDataGenerator()
         self.authentication_page = self.home_page.click_sign_in()
-        self.authentication_page.enter_create_account_email(self.data.EMAIL)
-        self.create_account_page = self.authentication_page.click_create_account()
+
 
     @data(*test_data.registration_data.get_csv_data("test_data/registration.csv"))
     @unpack
-    def testNoLastName(self, gender, firstname, lastname, email, password, day, month, year):
-        self.create_account_page.choose_gender(self.data.GENDER)
+    def testNoLastName(self, gender, firstname, _, email, password, day, month, year):
+        self.authentication_page.enter_create_account_email(email)
+        self.create_account_page = self.authentication_page.click_create_account()
+        self.create_account_page.choose_gender(gender)
         self.create_account_page.enter_first_name(firstname)
-        self.assertEqual(self.data.EMAIL, self.create_account_page.get_email_input())
+        self.assertEqual(email, self.create_account_page.get_email_input())
         self.create_account_page.enter_password(password)
-        self.create_account_page.select_date_of_birth(self.data.DATE_OF_BIRTH)
+        self.create_account_page.select_date_of_birth(datetime.date(int(year), int(month), int(day)))
         self.create_account_page.click_register_button()
         expected_number_of_errors_message = "There is 1 error"
         actual_number_of_errors_message = self.create_account_page.get_number_of_errors_message()
@@ -30,6 +34,8 @@ class RegistrationTest(BaseTest):
         self.assertCountEqual(expected_errors, visible_errors)
 
     def testNoFirstname(self):
+        self.authentication_page.enter_create_account_email(self.data.EMAIL)
+        self.create_account_page = self.authentication_page.click_create_account()
         self.create_account_page.choose_gender(self.data.GENDER)
         self.create_account_page.enter_last_name(self.data.LAST_NAME)
         self.assertEqual(self.data.EMAIL, self.create_account_page.get_email_input())
